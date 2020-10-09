e852457167e8b1a53302e1add74611470840fdab
Adernild <alexanderdernild@live.dk>
2020-10-09 20:12:58 +0200

    Initial repository commit

---
+++ Assignment_1.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 15:46:10 2020
+
+@author: Alexander Dernild
+"""
+#%% Importing libraries
+
+import os
+import re
+import math
+import matplotlib.pyplot as plt
+
+#%% 1) Pythagorean theorem
+
+def pythagorean_theorem(a, b, c):
+    unknown = 0
+    
+    if a == "?":
+        unknown = math.sqrt(int(c)**2 + int(b)**2)
+    elif b == "?":
+        unknown = math.sqrt(int(c)**2 + int(a)**2)
+    else:
+        unknown = math.sqrt(int(a)**2 + int(b)**2)
+    return unknown
+
+#%% test pythagorean_theorem
+
+print(pythagorean_theorem("4", "2", "?"))
+
+#%% 2) Median value
+
+def median(L):
+    L.sort()
+    middle = len(L)/2
+    if len(L) % 2 != 0:
+        return L[int(middle - .5)]
+    else:
+        return ((L[int(middle)] + L[int(middle-1)])/2)
+
+#%% test median
+
+L = [1, 5, 7, 2, 8, 9, 3, 6]
+
+print(median(L))
+
+#%% 3a) Unique list items (removing duplicates from list)
+
+def unique(L):
+    unique = []
+    for o in L:
+        if o not in unique:
+            unique.append(o)
+    return unique
+        
+#%% 3b) Unique list items 2 (returning only the unique elements)
+
+def unique2(L):
+    unique = []
+    for o in L:
+        if o in unique:
+            unique.remove(o)
+        else: 
+            unique.append(o)
+    return unique
+
+#%% Test unique and unique2
+
+L = ["hey", 0, 5, "hey", 6, 6, 7]
+
+print(unique(L))
+print(unique2(L))
+
+
+#%% 4) Character count
+
+def characters(textfile):
+    characters = {}
+    lines = open(textfile).read().split("\n")
+    for line in lines:
+        for ch in line:
+            if ch in characters.keys():
+                characters[ch] += 1
+            else:
+                characters[ch] = 1
+    for key in sorted(characters.keys()):
+        print(key, " :: ", characters[key])
+
+#%% Test characters
+
+print(characters("raven.txt"))
+
+#%% Helper function load_text
+
+def load_text(path):
+    file = open(path, "r", encoding = "UTF-8")
+    lines = []
+    for line in file:
+        line = line.lower()
+        line = re.sub('[^a-z\'\- ]','', line).strip()
+        lines.append(line)
+    file.close()
+    return lines
+
+#%% 5) Word Frequency
+
+def count(term):
+    dir = 'genesis'
+    files = sorted(os.listdir(dir))
+    frequencies = []
+    
+    for file in files:
+        word_counter = 0
+        lines = load_text('genesis/' + file)
+        for line in lines:
+            words = line.split(" ")
+            for word in words:
+                if word == term.lower():
+                    word_counter += 1
+        frequencies.append(word_counter)
+    fig = plt.figure(figsize=(15, 10)) 
+    plt.plot(list(range(1, len(frequencies) + 1)), frequencies)
+    fig.suptitle('Frequency of "' + term + '" in each chapter of genesis', fontsize=25)
+    plt.xlabel('Chapters', fontsize=20)
+    plt.ylabel('Frequency', fontsize=20)
+    fig.savefig('Frequency_plot.jpg')
+
+#%% Test count
+
+count("God")
---
+++ README.md
@@ -0,0 +1,36 @@
+# Assignment_1
+
+My solutions to the first assignment in DS800: Introduction to Data Processing
+
+## Assignment 1: Programming
+
+1) *Pythagorean theorem:* Write a program that solves the Pythagorean theorem.
+The user is asked to input two out of three side lengths a, b and c (for the unknown
+one, the user might enter "?"), and the program should calculate and print the
+length of the unknown side.
+
+2) *Median value:* Write a function median(L) that takes a list of numbers L and returns
+its median value. Sorting the list first helps to solve this task.
+
+3) *Unique list items:* Write a function unique(L) that takes a list of objects L and
+returns a sub-list that contains those items that occur only once in L.
+
+4) *Character count:* Write a function characters(textfile) that calculates the fre-
+quencies of characters in textfile. Use a dictionary to count how often each char-
+acter occurs. Finally, print the entire dictionary sorted alphabetically by keys.
+Test your function with the file raven.txt that should count, e.g., 109 occurrences
+of , (comma) and 17 occurrences of N. 
+
+5) *Word frequency:* The file genesis.zip contains text files for the 50 chapters of the
+Bible book Genesis. Write a function count(term) that analyzes the frequencies of
+term as follows:
+
+	1. initialize the variable frequencies
+	2. loop through the 50 chapters, for each chapter do the following:
+		i. open the corresponding TXT file
+		ii. load the text using the read function
+		iii. split text into words
+		iv. loop words and count how often a word equals term
+		v. add the result to frequencies
+	3. visualize frequencies with a line chart using matplotlib
+
